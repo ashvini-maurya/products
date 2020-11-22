e43bdf0499c4b627b50b59e4aaa5b95be7cf81f1
Ashvini Kumar <ashvini@esthenos.com>
2020-11-22 05:33:35 +0530

    infinite scroll added for dummy image API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
+import InfiniteScroll from "./components/InfiniteScroll";
 
 const formatNumber = (number) =>
   new Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(number);
@@ -71,69 +72,72 @@ class App extends Component {
     let total = 0;
 
     return (
-      <div className="product-list">
-        {this.state.loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <label className="search-products">Search Products</label>
+      <>
+        <div className="product-list">
+          {this.state.loading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <label className="search-products">Search Products</label>
 
-            <div className="labels">
-              <label className="first-label">Product Name</label>
-              <label className="second-label">Price Range</label>
-            </div>
+              <div className="labels">
+                <label className="first-label">Product Name</label>
+                <label className="second-label">Price Range</label>
+              </div>
 
-            <div>
-              <input
-                type="text"
-                value={this.state.search}
-                name="search"
-                placeholder="Search by Product"
-                onChange={(event) =>
-                  this.setState({ search: event.target.value })
-                }
-              />
-              {this.state.value?.max ? (
-                <InputRange
-                  minValue={0}
-                  maxValue={14}
-                  value={this.state.value}
-                  onChange={(value) => this.setState({ value })}
+              <div>
+                <input
+                  type="text"
+                  value={this.state.search}
+                  name="search"
+                  placeholder="Search by Product"
+                  onChange={(event) =>
+                    this.setState({ search: event.target.value })
+                  }
                 />
-              ) : null}
-            </div>
+                {this.state.value?.max ? (
+                  <InputRange
+                    minValue={0}
+                    maxValue={14}
+                    value={this.state.value}
+                    onChange={(value) => this.setState({ value })}
+                  />
+                ) : null}
+              </div>
 
-            <table>
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Unit Price</th>
-                  <th>Revenue</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products?.map((item, index) => {
-                  total = total + item.unitPrice * item.sold;
-                  return (
-                    <tr key={item.name}>
-                      <td>{item.name}</td>
-                      <td>{item.unitPrice}</td>
-                      <td>{formatNumber(item.unitPrice * item.sold)}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-              <tfoot>
-                <tr>
-                  <td>Total</td>
-                  <td></td>
-                  <td>{formatNumber(total)}</td>
-                </tr>
-              </tfoot>
-            </table>
-          </>
-        )}
-      </div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Product</th>
+                    <th>Unit Price</th>
+                    <th>Revenue</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products?.map((item, index) => {
+                    total = total + item.unitPrice * item.sold;
+                    return (
+                      <tr key={item.name}>
+                        <td>{item.name}</td>
+                        <td>{item.unitPrice}</td>
+                        <td>{formatNumber(item.unitPrice * item.sold)}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td>Total</td>
+                    <td></td>
+                    <td>{formatNumber(total)}</td>
+                  </tr>
+                </tfoot>
+              </table>
+            </>
+          )}
+        </div>
+        <InfiniteScroll />
+      </>
     );
   }
 }
---
+++ src/components/InfiniteScroll.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import axios from "axios";
+import "./InfiniteScroll.css";
+
+const InfiniteScroll = () => {
+  const [element, setElement] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState([]);
+
+  const page = useRef(1);
+  const prevY = useRef(0);
+
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const firstEntry = entries[0];
+        const y = firstEntry.boundingClientRect.y;
+        console.log(prevY.current, y); // check
+
+        if (prevY.current > y) {
+          setTimeout(() => loadMore(), 1000);
+        }
+        prevY.current = y; // update the current position
+      },
+      { threshold: 0.25 }
+    )
+  );
+
+  const fetchData = useCallback(async (pageNumber) => {
+    const url = `https://picsum.photos/v2/list?page=${pageNumber}&limit=15`;
+    setLoading(true);
+
+    try {
+      const res = await axios.get(url);
+      const { status, data } = res;
+
+      setLoading(false);
+      return { status, data };
+    } catch (e) {
+      setLoading(false);
+      return e;
+    }
+  }, []);
+
+  const handleInitial = useCallback(
+    async (page) => {
+      const newImages = await fetchData(page);
+      const { status, data } = newImages;
+      if (status === 200) setImages((images) => [...images, ...data]);
+    },
+    [fetchData]
+  );
+
+  const loadMore = () => {
+    page.current++;
+    handleInitial(page.current);
+  };
+
+  useEffect(() => {
+    handleInitial(page.current);
+  }, [handleInitial]);
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
+  return (
+    <div>
+      <p className="infinite-scroll">Infinite Scroll</p>
+
+      <div className="infiniteScroll">
+        {images &&
+          images.map((image, index) => (
+            <div key={index}>
+              <img
+                src={image.download_url}
+                alt={image.author}
+                className="imageStyle"
+              />
+            </div>
+          ))}
+
+        {loading && (
+          <div className="loadingSpinner">
+            <p>Spinner....!</p>
+          </div>
+        )}
+
+        <div ref={setElement} className="buttonContainer">
+          <button className="buttonStyle" onClick={loadMore}>
+            Load More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteScroll;
